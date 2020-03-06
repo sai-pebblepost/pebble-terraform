87ec31d69ecd066fe530ed37273cf410912fb33c
sai-pebblepost <61438125+sai-pebblepost@users.noreply.github.com>
2020-03-06 07:57:26 +0000

    Update terraform.tf

--- ec2-instances/app-server-blue/terraform.tf
+++ ec2-instances/app-server-blue/terraform.tf
@@ -1,1 +1,7 @@
-
+terraform {
+    backend "s3" {
+    bucket         = "pebble-terraform-tf-state"
+    key            = "pebble/flaskapp/blue/qa/terraform.tfstate"
+    region         = "us-east-1"
+  }
+ }
